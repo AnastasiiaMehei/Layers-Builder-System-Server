a18af7650dd88a26defd4d290a21614bfe3d4de5
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2025-01-23 21:37:30 +0200

    server

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import diagramsRouter from './routers/diagram.js';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const whitelist = [
   'http://localhost:5173',
@@ -13,36 +14,45 @@ const whitelist = [
 ];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-  };
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
 const PORT = Number(env('PORT', '3001'));
 
 export function setupServer() {
-    const app = express();
+  const app = express();
 
-    app.use(cors(corsOptions));
-    app.get('/', (req, res) => {
-      res.json({
-        message: 'Hello dear user!',
-      });
+  // Add proxy middleware
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://layers-builder-system-server.onrender.com',
+      changeOrigin: true,
+    })
+  );
+
+  app.use(cors(corsOptions));
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello dear user!',
     });
+  });
 
-    app.use('/uploads', express.static(UPLOAD_DIR));
-    app.use(cookieParser());
-    app.use(diagramsRouter);
-    app.use(notFoundHandler);
-    app.use(errorHandler);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use(cookieParser());
+  app.use(diagramsRouter);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
